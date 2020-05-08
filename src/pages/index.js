523e8f9c9d03dbc0e250ecc8bf8c68bd73c468cf
Faith Lee <faithghlee@gmail.com>
2020-05-07 22:59:23 -0400

    index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,18 @@ import indexStyle from "../styles/index.scss"
 const IndexPage = () => {
   return (
     <Layout>
-      <h1>Hi there, I am Faith Lee and welcome to my site. </h1>
+      <h1> Faith Lee</h1>
+      <h1> Data Scientist | Statistician </h1>
+      <p> I am a data scientist/statistician based in Quebec City, QC. I hold a master's degree in biostatistics and I have experience in analyzing data coming from different domains. I am very interested in the fields of survival analysis, longitudinal data analysis, machine learning and use of statistics in finance. I am also passionate in learning about computer science and the French language.</p>
+
+      <h1>Skills</h1>
+      <h2> Python, R, SAS, Java, HTML, CSS, LaTeX</h2>
+      <h2> English, Mandarin Chinese, French (intermediate-advance)</h2>
+
+      <h1>Experience</h1>
+      <h2> Data Scientist, MindBridge Analytics Inc. (Jun 2018 - Apr 2020)</h2>
+      <h2> Co-op Term, Institute for Clinical and Evaluative Sciences (May 2017 - Dec 2017)</h2>
+
     </Layout>
 
   )
